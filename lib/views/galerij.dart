7a8606c922a9f258476a309f1bb9033889f278a6
Prisonig <younessimouh@hotmail.com>
2021-12-13 11:12:29 +0100

    fixed

--- lib/views/galerij.dart
+++ lib/views/galerij.dart
@@ -6,7 +6,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:project_c/widgets/imageholder.dart';
 import 'package:project_c/widgets/navbar.dart';
-import 'package:project_c/classes/artwork.dart';
+
 
 
 class galerij extends StatefulWidget {
@@ -17,17 +17,10 @@ class galerij extends StatefulWidget {
 }
 
 class _galerijState extends State<galerij> {
-  int _counter = 0;
-    final Stream<QuerySnapshot> databaseart = FirebaseFirestore.instance.collection('loods_art').snapshots();
-    final List<Artworks> artworks=<Artworks>[
-    new Artworks('assets/sample_art.jpg','Hier komt de informatie over de prachtige kunstwerken.','Mooi Kunstwerk'),
-    new Artworks('assets/monalisa.jpeg', 'De Mona Lisa! Te vinden in Parijs.', 'Mona Lisa'),
-    new Artworks('assets/vrouw.jpg', 'Wanneer je die jonko flink op zn flikker geeft', 'Realiteit'),
-    ];
+  final Stream<QuerySnapshot> databaseart = FirebaseFirestore.instance.collection('loods_art').snapshots();
+
   @override
   Widget build(BuildContext context) {
-    final Artworksmap= artworks.asMap();
-    int count= Artworksmap.length;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -64,18 +57,6 @@ class _galerijState extends State<galerij> {
       bottomNavigationBar: navbar(),
     );
   }
-  void _incrementCounter() {
-    DatabaseReference _testRef = FirebaseDatabase.instance.reference().child("test");
-    _testRef.set("Hello World ${Random().nextInt(100)}");
 
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 }
 
